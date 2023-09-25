42b47c6c6e1b44822c98d01a6c164d8f8e185c1a
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-25 21:57:17 +0530

    convert int to string

---
+++ int-to-string.java
@@ -0,0 +1,9 @@
+ class inttostring
+ {  
+    public static void main(String args[])
+    {  
+        int i=200;  
+        String s=String.valueOf(i); 
+        System.out.println(s+100);  
+     }   
+}  
