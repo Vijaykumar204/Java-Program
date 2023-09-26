017801f24de71ef721c2b8930b42c4a201d64793
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-26 23:00:20 +0530

    convert float to string

---
+++ float-to-string.java
@@ -0,0 +1,9 @@
+class floattostring
+{  
+   public static void main(String args[])
+   {  
+             float f=12.3F;//F is the suffix for float  
+             String s=String.valueOf(f);  
+             System.out.println(s);  
+   }
+}  
