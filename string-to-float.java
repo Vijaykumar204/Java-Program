5b1ba3fd003e298e665815990e9311ec65f1face
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-26 22:59:23 +0530

    convert string to float

---
+++ string-to-float.java
@@ -0,0 +1,9 @@
+class stringtofloat
+{  
+        public static void main(String args[])
+        {  
+          String s="23.6";  
+          float f=Float.parseFloat("23.6");  
+          System.out.println(f);  
+        }
+}  
