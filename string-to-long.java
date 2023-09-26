ca999a5ddcd359d2c694e7f26cfff982a9cdf98a
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-26 23:02:25 +0530

    convert string to long

---
+++ string-to-long.java
@@ -0,0 +1,9 @@
+class stringtolong
+{  
+    public static void main(String args[]) 
+    {  
+           String s="9990449935";  
+           long l=Long.parseLong(s);  
+           System.out.println(l);  
+    }
+}
