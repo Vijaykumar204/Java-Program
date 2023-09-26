b40f8e1720fc164761d8a83ab6019162f07dc7ff
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-26 23:03:16 +0530

    convert long to string

---
+++ long-to-string.java
@@ -0,0 +1,9 @@
+class longtostring
+{  
+      public static void main(String args[])
+      {  
+           long i=9993939399L;  
+           String s=String.valueOf(i);  
+           System.out.println(s);  
+      }
+} 
