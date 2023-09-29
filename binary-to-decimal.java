027d5c515a6b58e07628c9867e05e7c46e7465af
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-09-29 19:41:22 +0530

    convert binary to decimal

---
+++ binary-to-decimal.java
@@ -0,0 +1,9 @@
+class binarytodecimal
+{  
+    public static void main(String args[])
+    {  
+                 String binaryString="1010";  
+                 int decimal=Integer.parseInt(binaryString,2);  
+                  System.out.println(decimal);  
+     }
+}  
