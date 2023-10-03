25cbd7cff2e4136625bd2407415a75574a09467d
Vijaykumar204 <vijayakumar2042003@gmail.com>
2023-10-03 22:04:15 +0530

    convert octal to decimal

---
+++ octal-to-decimal.java
@@ -0,0 +1,10 @@
+class octaltodecimal
+{  
+     public static void main(String args[])
+     {  
+
+            String octalString="121";  
+            int decimal=Integer.parseInt(octalString,8);  
+            System.out.println(decimal);  
+     }
+}  
